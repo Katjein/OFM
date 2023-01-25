a7bc8869badb58e13aec5eadea35f4d2ae07f04f
Katja <katjakorte@gmx.net>
2023-01-25 15:42:48 +0100

    CI Bands
    - Added Confidence Bands in all three planes
    - dynamic variable selection

--- data_analysis.m
+++ data_analysis.m
@@ -10,47 +10,136 @@ cd("../shoe\");
 load("normalized_data_shoe.mat");
 
 %%
-list_parameters = {'RTIBA'}; % list parameters that should be displayed % 'RFFHFA', 'RHFTBA','RFFTBA'
-for current_parameter = 1 : length(list_parameters)
-    % saggital
-    figure(1) % Dorsalflexion
-    subplot(ceil(length(list_parameters)), 1, current_parameter)
-    plot(barefoot.mean.(cell2mat(list_parameters(current_parameter)))(:, 1) )
-    hold on
-    plot(shoe.mean.(cell2mat(list_parameters(current_parameter)))(:, 1) )
-    title(list_parameters(current_parameter))
-    legend('barefoot', 'shoe')
+list_parameters = {'RTIBA', 'RFFHFA'}; % list parameters that should be displayed % 'RFFHFA', 'RHFTBA','RFFTBA'
+x = 1:1:length(barefoot.mean.LANA);
 
-    % transversal & Rotation
+for current_parameter = 1 : length(list_parameters)
+
+    % CONFIDENCE BANDS
+    for row = 1 : length(barefoot.interp.LANA.x(:, 1))
+        % BAREFOOT
+        barefoot.std.(cell2mat(list_parameters(current_parameter)))(row, 1) = ...
+            std(barefoot.interp.(cell2mat(list_parameters(current_parameter))).x(row, :));
+        barefoot.std.(cell2mat(list_parameters(current_parameter)))(row, 2) = ...
+            std(barefoot.interp.(cell2mat(list_parameters(current_parameter))).y(row, :));
+        barefoot.std.(cell2mat(list_parameters(current_parameter)))(row, 3) = ...
+            std(barefoot.interp.(cell2mat(list_parameters(current_parameter))).z(row, :));
+        % lower CI
+        barefoot.CI.lower.(cell2mat(list_parameters(current_parameter)))(row, 1) = ...
+            barefoot.mean.(cell2mat(list_parameters(current_parameter)))(row, 1) - (1.96 *barefoot.std.(cell2mat(list_parameters(current_parameter)))(row, 1));
+        barefoot.CI.lower.(cell2mat(list_parameters(current_parameter)))(row, 2) = ...
+            barefoot.mean.(cell2mat(list_parameters(current_parameter)))(row, 2) - (1.96 *barefoot.std.(cell2mat(list_parameters(current_parameter)))(row, 2));
+        barefoot.CI.lower.(cell2mat(list_parameters(current_parameter)))(row, 3) = ...
+            barefoot.mean.(cell2mat(list_parameters(current_parameter)))(row, 3) - (1.96 *barefoot.std.(cell2mat(list_parameters(current_parameter)))(row, 3));
+        % upper CI
+        barefoot.CI.upper.(cell2mat(list_parameters(current_parameter)))(row, 1) = ...
+            barefoot.mean.(cell2mat(list_parameters(current_parameter)))(row, 1) + (1.96 *barefoot.std.(cell2mat(list_parameters(current_parameter)))(row, 1));
+        barefoot.CI.upper.(cell2mat(list_parameters(current_parameter)))(row, 2) = ...
+            barefoot.mean.(cell2mat(list_parameters(current_parameter)))(row, 2) + (1.96 *barefoot.std.(cell2mat(list_parameters(current_parameter)))(row, 2));
+        barefoot.CI.upper.(cell2mat(list_parameters(current_parameter)))(row, 3) = ...
+            barefoot.mean.(cell2mat(list_parameters(current_parameter)))(row, 3) + (1.96 *barefoot.std.(cell2mat(list_parameters(current_parameter)))(row, 3));
+        
+        % SHOE
+        shoe.std.(cell2mat(list_parameters(current_parameter)))(row, 1) = ...
+            std(shoe.interp.(cell2mat(list_parameters(current_parameter))).x(row, :));
+        shoe.std.(cell2mat(list_parameters(current_parameter)))(row, 2) = ...
+            std(shoe.interp.(cell2mat(list_parameters(current_parameter))).y(row, :));
+        shoe.std.(cell2mat(list_parameters(current_parameter)))(row, 3) = ...
+            std(shoe.interp.(cell2mat(list_parameters(current_parameter))).z(row, :));
+        % lower CI
+        shoe.CI.lower.(cell2mat(list_parameters(current_parameter)))(row, 1) = ... % X
+            shoe.mean.(cell2mat(list_parameters(current_parameter)))(row, 1) - (1.96 *shoe.std.(cell2mat(list_parameters(current_parameter)))(row, 1)); 
+        shoe.CI.lower.(cell2mat(list_parameters(current_parameter)))(row, 2) = ... % Y
+            shoe.mean.(cell2mat(list_parameters(current_parameter)))(row, 2) - (1.96 *shoe.std.(cell2mat(list_parameters(current_parameter)))(row, 2)); 
+        shoe.CI.lower.(cell2mat(list_parameters(current_parameter)))(row, 3) = ... % Z
+            shoe.mean.(cell2mat(list_parameters(current_parameter)))(row, 3) - (1.96 *shoe.std.(cell2mat(list_parameters(current_parameter)))(row, 3));
+        % upper CI
+        shoe.CI.upper.(cell2mat(list_parameters(current_parameter)))(row, 1) = ... % X
+            shoe.mean.(cell2mat(list_parameters(current_parameter)))(row, 1) + (1.96 *shoe.std.(cell2mat(list_parameters(current_parameter)))(row, 1)); 
+        shoe.CI.upper.(cell2mat(list_parameters(current_parameter)))(row, 2) = ... % Y
+            shoe.mean.(cell2mat(list_parameters(current_parameter)))(row, 2) + (1.96 *shoe.std.(cell2mat(list_parameters(current_parameter)))(row, 2)); 
+        shoe.CI.upper.(cell2mat(list_parameters(current_parameter)))(row, 3) = ... % Z
+            shoe.mean.(cell2mat(list_parameters(current_parameter)))(row, 3) + (1.96 *shoe.std.(cell2mat(list_parameters(current_parameter)))(row, 3));
+    end
+
+    % PLOTS
+    % saggital = Dorsalflexion
+    figure(1)
+    subplot(ceil(length(list_parameters)), 1, current_parameter)
+    hold on
+    % barefoot
+        patch([x fliplr(x)], ...
+            [barefoot.CI.lower.(cell2mat(list_parameters(current_parameter)))(:, 1)', ... % lower limit
+            fliplr(barefoot.CI.upper.(cell2mat(list_parameters(current_parameter)))(:, 1)')], ... % upper limit
+            [0, 0.4470, 0.7410], ... % color
+            FaceAlpha=0.3, ...     % intensity
+            EdgeColor='none');
+        plot(barefoot.mean.(cell2mat(list_parameters(current_parameter)))(:, 1) )
+    %shoe
+        patch([x fliplr(x)], ...
+            [shoe.CI.lower.(cell2mat(list_parameters(current_parameter)))(:, 1)', ... % lower limit
+            fliplr(shoe.CI.upper.(cell2mat(list_parameters(current_parameter)))(:, 1)')], ... % upper limit
+            [0.8500 0.3250 0.0980], ... % color
+            FaceAlpha=0.3, ...     % intensity
+            EdgeColor='none');
+        plot(shoe.mean.(cell2mat(list_parameters(current_parameter)))(:, 1) )
+    title(list_parameters(current_parameter))
+    subtitle('Saggital Plane')
+    legend('CI barefoot', 'barefoot mean', 'CI shoe', 'shoe mean')
+    xlabel('Gait Cycle (%)')
+    ylabel('Dorsiflexion (deg)')
+
+    % transversal = Rotation
     figure(2) 
     subplot(ceil(length(list_parameters)), 1, current_parameter)
-    plot(barefoot.mean.(cell2mat(list_parameters(current_parameter)))(:, 2) )
     hold on
-    plot(shoe.mean.(cell2mat(list_parameters(current_parameter)))(:, 2) )
+        patch([x fliplr(x)], ...
+            [barefoot.CI.lower.(cell2mat(list_parameters(current_parameter)))(:, 2)', ... % lower limit
+            fliplr(barefoot.CI.upper.(cell2mat(list_parameters(current_parameter)))(:, 2)')], ... % upper limit
+            [0, 0.4470, 0.7410], ... % color
+            FaceAlpha=0.3, ...     % intensity
+            EdgeColor='none');
+        plot(barefoot.mean.(cell2mat(list_parameters(current_parameter)))(:, 2) )
+    % shoe
+        patch([x fliplr(x)], ...
+            [shoe.CI.lower.(cell2mat(list_parameters(current_parameter)))(:, 2)', ... % lower limit
+            fliplr(shoe.CI.upper.(cell2mat(list_parameters(current_parameter)))(:, 2)')], ... % upper limit
+            [0.8500 0.3250 0.0980], ... % color
+            FaceAlpha=0.3, ...     % intensity
+            EdgeColor='none');
+        plot(shoe.mean.(cell2mat(list_parameters(current_parameter)))(:, 2) )
     title(list_parameters(current_parameter))
-    legend('barefoot', 'shoe')
+    subtitle('Transversal Plane')
+    legend('CI barefoot', 'barefoot mean', 'CI shoe', 'shoe mean')
+    xlabel('Gait Cycle (%)')
+    ylabel('Dorsiflexion (deg)')
 
-    % frontal & Pronation/Supination
+    % frontal = Pronation/Supination
     figure(3) 
     subplot(ceil(length(list_parameters)), 1, current_parameter)
-    plot(barefoot.mean.(cell2mat(list_parameters(current_parameter)))(:, 3) )
     hold on
-    plot(shoe.mean.(cell2mat(list_parameters(current_parameter)))(:, 3) )
+    % barefoot
+        patch([x fliplr(x)], ...
+            [barefoot.CI.lower.(cell2mat(list_parameters(current_parameter)))(:, 3)', ... % lower limit
+            fliplr(barefoot.CI.upper.(cell2mat(list_parameters(current_parameter)))(:, 3)')], ... % upper limit
+            [0, 0.4470, 0.7410], ... % color
+            FaceAlpha=0.3, ...     % intensity
+            EdgeColor='none');
+        plot(barefoot.mean.(cell2mat(list_parameters(current_parameter)))(:, 3) )
+    % shoe
+        patch([x fliplr(x)], ...
+            [shoe.CI.lower.(cell2mat(list_parameters(current_parameter)))(:, 3)', ... % lower limit
+            fliplr(shoe.CI.upper.(cell2mat(list_parameters(current_parameter)))(:, 3)')], ... % upper limit
+            [0.8500 0.3250 0.0980], ... % color
+            FaceAlpha=0.3, ...     % intensity
+            EdgeColor='none');
+        plot(shoe.mean.(cell2mat(list_parameters(current_parameter)))(:, 3) )
     title(list_parameters(current_parameter))
-    legend('barefoot', 'shoe')
+    subtitle('Frontal Plane')
+    legend('CI barefoot', 'barefoot mean', 'CI shoe', 'shoe mean')
+    xlabel('Gait Cycle (%)')
+    ylabel('Dorsiflexion (deg)')
 end
-
-
-%% CONFIDENCE BANDS
-for row = 1 : length(barefoot.interp.RTIBA.x(:, 1))
-    barefoot.std.RTIBA(row, :) = std(barefoot.interp.RTIBA.x(row, :));
-    barefoot.CI(row, 1) = barefoot.mean.RTIBA(row, 1) - (1.96 *barefoot.std.RTIBA(row)); % lower CI
-    barefoot.CI(row, 2) = barefoot.mean.RTIBA(row, 1) + (1.96 *barefoot.std.RTIBA(row)); % upper CI
-end
-
-x = 1 : 1 : 101;
-figure(1)
-patch([x fliplr(x)], [barefoot.CI(:, 1)' fliplr(barefoot.CI(:, 2)')], [0, 0.4470, 0.7410], FaceAlpha=0.3);
 
 
 % -> ganze Kurve vergleichen vs. Kennmale 
